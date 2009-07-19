813203508e16e99dd3381dc0da42111e91807bc6
qxjit (David Vollbracht) <david.vollbracht@gmail.com>
2009-07-18 22:52:54 -0400

    add any_live_wires? to switchboard to test whether there is anyone still connected

--- test/switchboard_test.rb
+++ test/switchboard_test.rb
@@ -15,6 +15,44 @@ module Telegraph
         end
       end
     end
+
+    context "any_live_wires?" do
+      setup do
+        @switchboard = Switchboard.new
+        @operator = Operator.listen "localhost", 9999, @switchboard
+      end
+
+      teardown do
+        @operator.shutdown if @operator
+      end
+
+      should "return false if no wires are connected" do
+        assert_equal false, @switchboard.any_live_wires?
+      end
+
+      should "return true if there is a live wire connected" do
+        begin
+          w = Wire.connect "localhost", 9999
+          w.send_message :open
+          @switchboard.next_message :timeout => 0.1
+          assert_equal true, @switchboard.any_live_wires?
+        ensure
+          w.close if w
+        end
+      end
+
+      should "return false if a closed wire is connected" do
+        begin
+          w = Wire.connect "localhost", 9999
+          w.send_message :open
+          @switchboard.next_message :timeout => 0.1
+        ensure
+          w.close if w
+        end
+        @switchboard.next_message :timeout => 0.1 rescue NoMessageAvailable
+        assert_equal false, @switchboard.any_live_wires?
+      end
+    end
   end
 end
 
